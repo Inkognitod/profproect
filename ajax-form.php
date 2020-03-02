6cdc1099e95dc4cd7d66e24f39ade5418e22ca02
Inkognitod <inkognitod@mail.ru>
2020-03-02 11:06:08 +0300

    1.5
    мелкие правки
    добавлено уведомление в телеграм

--- ajax-form.php
+++ ajax-form.php
@@ -11,6 +11,8 @@ $email = $_POST['email'];
 
 //Теперь давайте настроим куда отправляем и откуда
 
+/*
+$my_email = 'inkognitod@mail.ru'; // Куда отправляем*/
 $my_email = 'profproect@bk.ru'; // Куда отправляем
 $sender_email = '<profproect@bk.ru>'; // От кого отправляем
 $title = "Заявка с сайта profproect116.ru"; 
@@ -26,6 +28,22 @@ $mes = "
 //Всё, теперь можно отправлять письмо на почту
 
 $send = mail ($my_email,$title,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$sender_email");
+
+// подключаем телеграм
+$token = "954131762:AAGWSeQD0sWYmWYtJi3H-dd1p6on4JbNGZE";
+$chat_id = "-433339781";
+$arr = array(
+    'Имя' => $name,
+    'Телефон' => $phone,
+    'Почта' => $email
+  );
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+
+
 
 }
 
